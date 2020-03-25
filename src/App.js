a4c932e541e439f2efb1e0ee2f0c21c65ad28fa0
karlas <carles.pages.rozas@gmail.com>
2020-03-25 20:41:08 +0100

    Fade in with animation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled, { css, keyframes } from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const fadeInAnimation = keyframes`
+  0% { opacity: 0 }
+  100% { opacity: 1 }
+`
 
-export default App;
+const fadeIn = time => css`
+  animation-name: ${ fadeInAnimation };
+  animation-duration: ${ time }s;
+`
+
+const Circle = styled.div`
+  ${ fadeIn(2) }
+  height: 100px;
+  width: 100px;
+  border-style: solid;
+  border-width: 5px;
+  border-radius: 50%;
+  border-color: black;
+`
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 450px;
+`
+
+export default () => (
+  <Container>
+    <Circle />
+  </Container>
+)
